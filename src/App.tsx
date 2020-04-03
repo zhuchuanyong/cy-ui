c967418e9ab5302b44dee794f4348a79af03b36c
zhuchuanyong <zhuchuanyong@vtstar.net>
2020-04-03 23:22:46 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, Buttonsize } from './components/Button/Button';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button autoFocus >hello</Button>
+        <Button onClick={(e) => { e.preventDefault(); alert(123) }} >hello</Button>
+        <Button disabled>disabled button</Button>
+        <Button btnType={ButtonType.Primary} size={Buttonsize.large}>Primary  large</Button>
+        <Button btnType={ButtonType.Danger} size={Buttonsize.Small}>Danger  Small</Button>
+        <Button btnType={ButtonType.Link} href="www.baidu.com">百度</Button>
+        <Button disabled btnType={ButtonType.Link} href="www.baidu.com">百度</Button>
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import classnames from 'classnames';
+import { type } from 'os';
+export enum Buttonsize {
+  large = 'lg',
+  Small = 'sm'
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link'
+}
+
+interface BaseButtonProps {
+  className?: string,
+  disabled?: boolean,
+  size?: Buttonsize,
+  btnType?: ButtonType,
+  children: React.ReactNode,
+  href?: string
+}
+
+// button 属性和原生button属性
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
+// a 属性和原生a属性
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+
+// Partial<> 把a和button属性设置为可选
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const {
+    btnType,
+    disabled,
+    className,
+    size,
+    children,
+    href,
+    ...restProps
+  } = props
+  console.log(props);
+
+  // btn , btn-lg
+  const classs = classnames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === ButtonType.Link) && disabled
+  })
+  console.log(classs);
+
+  if (btnType === ButtonType.Link && href) {
+    return (
+      <a className={classs} href={href} {...restProps}>
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <button className={classs} disabled={disabled} {...restProps}>
+        {children}
+      </button>
+    )
+  }
+}
+Button.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
+}
+export default Button
