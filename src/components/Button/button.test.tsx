92accb218b61e52d1b09385b4a5d60c775034365
zhuchuanyong <zhuchuanyong@vtstar.net>
2020-04-06 17:28:43 +0800

    添加button测试代码

---
+++ src/components/Button/button.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Button from './Button'
+
+const defaultProps = {
+  onClick: jest.fn()
+}
+describe('test 组件button', () => {
+  it('默认', () => {
+    const wrapper = render(<Button {...defaultProps}>Nice</Button>)
+    const element = wrapper.queryByText('Nice')
+
+    expect(element).toBeInTheDocument()
+    expect(element?.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn  btn-default')
+    fireEvent.click(element)
+    expect(defaultProps.onClick).toHaveBeenCalled()
+  })
+})
